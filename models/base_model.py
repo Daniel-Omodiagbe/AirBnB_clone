c1f4e9b01603752155028555ba9c815af1d73631
Daniel-Omodiagbe <danielomodiagbe26@gmail.com>
2024-05-18 16:18:27 +0100

    Hotfix

--- models/base_model.py
+++ models/base_model.py
@@ -49,5 +49,4 @@ class BaseModel:
         dict_1['created_at'] = self.created_at.isoformat()
         dict_1['updated_at'] = self.updated_at.isoformat()
         dict_1["__class__"] = self.__class__.__name__
-        if dict_1:
-            return dict_1
+        return dict_1
